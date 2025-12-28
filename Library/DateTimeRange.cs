c0b2a668451b8a613ca56117ce0522b7cf517e2a
Alexander <resnyanskiy@gmail.com>
2025-12-28 21:01:08 +0600

    Factory `DateTimeRange.Create`.

--- Library/DateTimeRange.cs
+++ Library/DateTimeRange.cs
@@ -2,6 +2,37 @@
 
 public record struct DateTimeRange
 {
+	public static IEnumerable<DateTimeRange> Create<T>(IEnumerable<KeyValuePair<DateTime, T>> values, T threshold)
+		where T : IComparable<T>
+	{
+		using var enumerator = values.GetEnumerator();
+
+		while (enumerator.MoveNext())
+		{
+			// начать интервал, если значение больше порога
+			if (enumerator.Current.Value.CompareTo(threshold) > 0)
+			{
+				var begin = enumerator.Current.Key;
+				var end = DateTime.MaxValue;
+				var available = false;
+
+				do
+				{
+					if (available = enumerator.MoveNext())
+					{
+						end = enumerator.Current.Key;
+						if (end < begin)
+							throw new ArgumentException("Dictionary must be sorted by key.", nameof(values));
+					}
+				}
+				// продлевать интервал, если значение больше порога
+				while (available && enumerator.Current.Value.CompareTo(threshold) > 0);
+
+				yield return new DateTimeRange(begin, end);
+			}
+		}
+	}
+	
 	public DateTimeRange(DateTime? begin, DateTime? end)
 	{
 		if (begin != null && end != null && end < begin)
--- Tests/FactoryTests.cs
+++ Tests/FactoryTests.cs
@@ -2,33 +2,103 @@ namespace DateTimeRangeLibrary;
 
 public class FactoryTests
 {
-	[Theory, ClassData(typeof(EdgeCasesData))]
-	public void Constructor_WithEdgeCases(DateTime begin, TimeSpan duration, DateTime expectedBegin, DateTime expectedEnd)
+	[Fact]
+	public void CreateFromPulse()
 	{
+		/*
+		 * input:    + - + - + + - -
+		 * output:   |-| |-| |---|
+		 */
+
+		// Arrange
+		var begin = DateTime.Today;
+		var pulse = new Dictionary<DateTime, bool>
+		{
+			[begin.AddMinutes(1)] = true,
+			[begin.AddMinutes(2)] = false,
+			//
+			[begin.AddMinutes(3)] = true,
+			[begin.AddMinutes(4)] = false,
+			//
+			[begin.AddMinutes(5)] = true,
+			[begin.AddMinutes(6)] = true,
+			[begin.AddMinutes(7)] = false,
+			[begin.AddMinutes(8)] = false
+		};
+
 		// Act
-		var range = DateTimeRange.Create(begin, duration);
+		var ranges = DateTimeRange.Create(pulse, false).ToArray();
 
 		// Assert
-		Assert.Equal(expectedBegin, range.Begin);
-		Assert.Equal(expectedEnd, range.End);
+		Assert.Equal(3, ranges.Length);
+		Assert.Equal(new DateTimeRange(begin.AddMinutes(1), begin.AddMinutes(2)), ranges[0]);
+		Assert.Equal(new DateTimeRange(begin.AddMinutes(3), begin.AddMinutes(4)), ranges[1]);
+		Assert.Equal(new DateTimeRange(begin.AddMinutes(5), begin.AddMinutes(7)), ranges[2]);
 	}
 
-	private class EdgeCasesData : TheoryData<DateTime, TimeSpan, DateTime, DateTime>
+	[Fact]
+	public void CreateFromSignal_If_NoGoodSignal_Then_NoRanges()
 	{
-		public EdgeCasesData()
+		// Arrange
+		var begin = DateTime.Today;
+		const int threshold = 3;
+		var signal = new Dictionary<DateTime, int>()
 		{
-			// защита от перехода через DateTime.MaxValue
-			Set(
-				begin: new DateTime(1), duration: TimeSpan.MaxValue,
-				expectedBegin: new DateTime(1), expectedEnd: DateTime.MaxValue);
-
-			// защита от перехода через DateTime.MinValue
-			Set(
-				begin: new DateTime(1), duration: new TimeSpan(-2),
-				expectedBegin: DateTime.MinValue, expectedEnd: new DateTime(1));
-		}
-
-		private void Set(DateTime begin, TimeSpan duration, DateTime expectedBegin, DateTime expectedEnd)
-			=> Add(begin, duration, expectedBegin, expectedEnd);
+			[begin.AddMinutes(1)] = 1,
+			[begin.AddMinutes(2)] = 2,
+			[begin.AddMinutes(3)] = 3,
+			[begin.AddMinutes(4)] = 2,
+		};
+		
+		// Act
+		var ranges = DateTimeRange.Create(signal, threshold).ToArray();
+		
+		// Assert
+		Assert.Empty(ranges);
+	}
+	
+	[Fact]
+	public void CreateFromSignal_If_NoEndSignal_Then_LastSignalIsRangeEnd()
+	{
+		// Arrange
+		var begin = DateTime.Today;
+		const int threshold = 3;
+		var signal = new Dictionary<DateTime, int>()
+		{
+			[begin.AddMinutes(1)] = 1,
+			[begin.AddMinutes(2)] = 2,
+			[begin.AddMinutes(3)] = 5,
+			[begin.AddMinutes(4)] = 4,
+			[begin.AddMinutes(5)] = 4,
+		};
+		
+		// Act
+		var ranges = DateTimeRange.Create(signal, threshold).ToArray();
+		
+		// Assert
+		Assert.Single(ranges);
+		Assert.Equal(begin.AddMinutes(3), ranges[0].Begin);
+		Assert.Equal(begin.AddMinutes(5), ranges[0].End);		
+	}
+	
+	[Fact]
+	public void CreateFromSignal_If_OneSignal_Then_RangeWithNoEnd()
+	{
+		// Arrange
+		var begin = DateTime.Today;
+		const int threshold = 3;
+		var signal = new Dictionary<DateTime, int>()
+		{
+			[begin.AddMinutes(1)] = 4,
+			[begin.AddMinutes(2)] = 2,
+			[begin.AddMinutes(3)] = 5,
+		};
+		
+		// Act
+		var ranges = DateTimeRange.Create(signal, threshold).ToArray();
+		
+		// Assert
+		Assert.Equal(begin.AddMinutes(3), ranges[1].Begin);
+		Assert.Equal(DateTime.MaxValue, ranges[1].End);		
 	}
 }
